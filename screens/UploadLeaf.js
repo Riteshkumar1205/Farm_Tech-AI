8d2e8cd66b34b66f9f8279cb206fddfce07d008a
Ritesh Kumar <glbitm2025@gmail.com>
2025-09-04 14:49:59 +0530

    Create UploadLeaf.js

---
+++ screens/UploadLeaf.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function UploadLeaf() {
+  return (
+    <View>
+      <Text>Upload Leaf for Disease Detection (placeholder)</Text>
+    </View>
+  );
+}
